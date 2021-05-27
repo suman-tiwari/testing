48663df09bc620818f0cd206823c473e29e298c2
suman <tiwarisuman80@gmail.com>
2021-05-27 17:51:24 +0545

    test cases success using capybara

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, driver: :selenium_chrome, js: true do
-	describe 'create' do
+	describe 'create post' do
 		it 'should create a post' do
 			visit new_post_path
 			fill_in 'Title', with: "My Post"
@@ -9,6 +9,24 @@ RSpec.describe Post, driver: :selenium_chrome, js: true do
 
 			click_button 'Create Post'
 			expect(page).to have_content 'Post was successfully created.'
+		end
+	end
+
+	describe 'list post' do
+		it 'should list posts' do
+			visit posts_path
+			expect(page).to have_content 'My Post My Content'
+		end
+	end
+
+	describe 'update post' do
+		it 'should update existing post' do
+			visit edit_post_path(Post.first)
+			fill_in 'Title', with: "Edited Title"
+			fill_in 'Content', with: "Edited content"
+
+			click_button "Update Post"
+			expect(page).to have_content "Post was successfully updated."
 		end
 	end
 end
